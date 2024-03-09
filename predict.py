3afb13b6da634277812626594d42c2aefdf6bd46
Varsha <varshakt6@gmail.com>
2024-03-09 20:09:19 +0530

    pdf revert

--- predict.py
+++ predict.py
@@ -50,10 +50,10 @@ def main():
             if file_extension == 'pdf':
                 pdf_bytes = uploaded_file.read()           
                 pdf_base64 = base64.b64encode(pdf_bytes).decode('utf-8')
-                pdf_viewer(pdf_bytes,width=700,height=500)
-                # pdf_display = F'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" )height="500" type="application/pdf"></iframe>'
+                # pdf_viewer(pdf_bytes)
+                pdf_display = F'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="500" type="application/pdf"></iframe>'
                 # # st.markdown(f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="500" type="application/pdf" style="border: none;"></iframe>', unsafe_allow_html=True)
-                # st.markdown(pdf_display, unsafe_allow_html=True)
+                st.markdown(pdf_display, unsafe_allow_html=True)
                 pdf = PyPDF2.PdfReader(uploaded_file)
                 
                 for i in range(len(pdf.pages)):
